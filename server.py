2f678fc969ca9378a567bd1f2118a0015a64e140
MadhuriHB <madhurimahajan@yahoo.com>
2016-08-12 19:09:58 +0000

    get data from panaramio api, numbeo api

--- server.py
+++ server.py
@@ -7,11 +7,16 @@ from xml.dom.minidom import parse, parseString
 
 from model import connect_to_db, db, School
 from pyzipcode import ZipCodeDatabase
+#import geopy
+
+#import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
 
 import requests
 import os
 
-import untangle
 
 app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
@@ -24,7 +29,7 @@ zcdb = ZipCodeDatabase()
 app.jinja_env.undefined = StrictUndefined
 API_KEY = os.environ.get("API_KEY")
 API_KEY_GS = os.environ.get("api_key_gs")
-
+API_KEY_NUMBEO = os.environ.get("API_KEY_NUMBEO")
 
 @app.route('/')
 def index():
@@ -43,12 +48,34 @@ def show_city_info():
 
 
 @app.route("/show_city/<int:zipcode>")
-def show_city_on_map(zipcode):
-    pass
-    resp = requests.get("http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20&minx=-34&miny=-118&maxx=34&maxy=118&size=medium&mapfilter=true")
-    print "This is the response!!!!!!!!!!!!!", resp.json()
+def show_city_images(zipcode):
+    zipcode = zcdb[zipcode]
+    lat = zipcode.latitude
+    lon = zipcode.longitude
+    min_lat = float(lat) - 0.01
+    min_lon = float(lon) - 0.01
+    max_lat = float(lat) + 0.01
+    max_lon = float(lon) + 0.01
+    print "LATITUDE ", lat
+    print "LONGITUDE!!!!!!!!!!", lon
+    resp = requests.get(
+        "http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=10&minx=%s&miny=%s&maxx=%s&maxy=%s&size=medium&mapfilter=true"
+        % (min_lon, min_lat, max_lon, max_lat))
+    resp = resp.json()
+    photos = resp['photos']
+    photoUrls = []
+    images = []
+    for photo in photos:
+        url = photo['photo_url']
+        photoUrls.append(url)
+        page = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(page, "html.parser")
+        image = soup.find(id="main-photo_photo", src=True)
+        images.append(image)
+        #print "Image is here!!!!!!!!!!!!", image
 
-    return render_template("show_map.html", zipcode=zipcode)
+    #print "These are the url_list !!!!!!!!!!!!!", photoUrls
+    return render_template("show_map.html", images=images)
 
 
 @app.route("/show_school/<int:zipcode>")
@@ -58,7 +85,7 @@ def show_school_ratings(zipcode):
     zipcode = zcdb[zipcode]
     # latitude = zipcode.latitude
     # longitude = zipcode.longitude
-    city = zipcode.city
+    #city = zipcode.city
     state = zipcode.state
     zip1 = zipcode.zip
 
@@ -108,6 +135,78 @@ def show_school_ratings(zipcode):
     #     # xml_SchoolProfile = requests.get(
     #     #     "http://api.greatschools.org/schools/%s/%s?key=%s" 
     #     #     % (state, gsid, API_KEY_GS))       
+
+
+def find_nearest_city(zipcode):
+    """finds the nearest big city for the user city
+       to find cost of living
+    """
+    
+    from geopy.distance import vincenty
+    zipcode = zcdb[zipcode]
+    myCity = zipcode.city
+    #myState = zipcode.state
+    myCitylat = zipcode.latitude
+    myCitylon = zipcode.longitude
+    
+    #stores all city objects in US 
+    cityObjects = []
+
+    # get all cities from numbeo api call 
+    resp = requests.get("http://numbeo.com/api/cities?api_key="+API_KEY_NUMBEO)
+   
+    resp = resp.json()
+
+    for city_dict in resp.values()[0]:
+        # get cities from US only
+        if city_dict['country'] == 'United States':  
+            cityObjects.append(city_dict)
+
+    nearestCityObject = None
+    nearestDistance = 1000
+
+    #city for the zipcode that user entered
+    myCity = (myCitylat, myCitylon)
+
+    for cityObject in cityObjects:
+
+        #city lat and long from citiobject that we are comapring
+        isNearCity = (cityObject['latitude'], cityObject['longitude']) 
+        
+        #geopy function vincenty to calculate the distance
+        distance = vincenty(myCity, isNearCity).miles  
+        if distance < nearestDistance:
+            nearestDistance = distance
+            nearestCityObject = cityObject
+
+    #print nearestCityObject      
+
+    return nearestCityObject
+
+
+@app.route("/cost_of_living/<int:zipcode>")
+def show_cost_of_living(zipcode):
+    """calculate cost of living for city"""
+    #import pdb; pdb.set_trace()
+    city = find_nearest_city(int(zipcode))
+    print "This is city object!!!!!!!", city
+    resp = requests.get("http://numbeo.com/api/city_prices?api_key=%s&city_id=%s" % (API_KEY_NUMBEO, city['city_id']))
+    resp = resp.json()
+    prices = resp['prices']
+
+    return render_template("cost_of_living.html", prices)
+
+
+@app.route("/crime_rate/<int:zipcode>")
+def show_crime_rate(zipcode):
+    """show crime rate for the city"""
+    #import pdb; pdb.set_trace()
+    city = find_nearest_city(zipcode)
+    resp = requests.get("http://numbeo.com//api/city_crime?api_key=%s&city_id=%s" % (API_KEY_NUMBEO, city['city_id']))
+    crime_data = resp.json()
+
+    return render_template("crime_rate.html", crime_data=crime_data)
+
 
 
 if __name__ == "__main__":
